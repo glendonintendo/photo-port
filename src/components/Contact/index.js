7daf779fab1deb1c0d0f2a1e130202d36c59ef4b
glendo <gleneggers64@gmail.com>
2021-06-17 15:13:57 -0500

    add client-side validation to form with errorMessage state variable

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,11 +1,31 @@
 import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
 	const [formState, setFormState] = useState({ name: '', email: '', message: ''});
 	const { name, email, message } = formState;
+
+	const [errorMessage, setErrorMessage] = useState('');
 	
-	const handleChange = (e) => {
-		setFormState({...formState, [e.target.name]: e.target.value });
+	const handleBlur = (e) => {
+		if (e.target.name === 'email') {
+			const isValid = validateEmail(e.target.value);
+			if (!isValid) {
+				setErrorMessage('Your email is invalid');
+			} else {
+				setErrorMessage('');
+			}
+		} else {
+			if (!e.target.value.length) {
+				setErrorMessage(`${e.target.name} is required.`);
+			} else {
+				setErrorMessage('');
+			}
+		}
+
+		if (!errorMessage) {
+			setFormState({...formState, [e.target.name]: e.target.value });
+		}
 	};
 
 	const handleSubmit = (e) => {
@@ -24,7 +44,7 @@ function ContactForm() {
 						type ='text' 
 						name='name' 
 						default={name} 
-						onChange={handleChange}					
+						onBlur={handleBlur}					
 					/>
 				</div>
 
@@ -34,7 +54,7 @@ function ContactForm() {
 						type='email' 
 						name='email' 
 						default={email} 
-						onChange={handleChange}	
+						onBlur={handleBlur}	
 					/>
 				</div>
 
@@ -44,9 +64,15 @@ function ContactForm() {
 						name='message' 
 						rows='5' 
 						default={message} 
-						onChange={handleChange}	
+						onBlur={handleBlur}	
 					/>
 				</div>
+
+				{errorMessage && (
+					<div>
+						<p className='error-text'>{errorMessage}</p>
+					</div>
+				)}
 
 				<button type='submit'>Submit</button>
 			</form>
