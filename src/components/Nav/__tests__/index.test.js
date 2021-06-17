2f0f29fc82a9e39f8c960b8d29afc687d2aace44
glendo <gleneggers64@gmail.com>
2021-06-17 15:27:06 -0500

    update tests with additional prop mocks

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -8,6 +8,8 @@ const categories = [
 ];
 const mockCurrentCategory = jest.fn();
 const mockSetCurrentCategory = jest.fn();
+const mockContactSelected = jest.fn();
+const mockSetContactSelected = jest.fn();
 
 afterEach(cleanup);
 
@@ -17,6 +19,8 @@ describe('Nav component', () => {
 			categories={categories}
 			setCurrentCategory={mockSetCurrentCategory}
 			currentCategory={mockCurrentCategory}
+			contactSelected={mockContactSelected}
+			setContactSelected={mockSetContactSelected}
 		/>);
 	});
 
@@ -25,6 +29,8 @@ describe('Nav component', () => {
 			categories={categories}
 			setCurrentCategory={mockSetCurrentCategory}
 			currentCategory={mockCurrentCategory}
+			contactSelected={mockContactSelected}
+			setContactSelected={mockSetContactSelected}
 		/>);
 
 		expect(asFragment()).toMatchSnapshot();
@@ -37,6 +43,8 @@ describe('emoji is visible', () => {
 			categories={categories}
 			setCurrentCategory={mockSetCurrentCategory}
 			currentCategory={mockCurrentCategory}
+			contactSelected={mockContactSelected}
+			setContactSelected={mockSetContactSelected}
 		/>);
 
 		expect(getByLabelText('camera')).toHaveTextContent('📸');
