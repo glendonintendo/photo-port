7073f3ba740dd5065dc13f76bb4b248feb797e05
glendo <gleneggers64@gmail.com>
2021-06-17 13:35:57 -0500

    add ContactForm component to App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import About from './components/About';
 import Nav from './components/Nav';
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact'; 
 
 function App() {
 	const [categories] = useState([
@@ -27,6 +28,7 @@ function App() {
 				currentCategory={currentCategory}
 			/>
 			<main>
+				<ContactForm />
 				<Gallery currentCategory={currentCategory} />
 				<About />
 			</main>
