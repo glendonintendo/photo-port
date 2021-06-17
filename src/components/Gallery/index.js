1eab5fae4f32029539068c240f60fdbe88273bec
glendo <gleneggers64@gmail.com>
2021-06-17 12:25:15 -0500

    use props; add PhotoList component to returned JSX

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,25 +1,15 @@
 import React from 'react';
 import { capitalizeFirstLetter } from '../../utils/helpers';
-import photo from '../../assets/small/commercial/0.jpg';
+import PhotoList from '../PhotoList';
 
-function Gallery() {
-	const currentCategory = {
-		name: "commercial",
-		description:
-		  "Photos of grocery stores, food trucks, and other commercial projects",
-	};
+function Gallery({ currentCategory }) {
+	const { name, description } = currentCategory;
 	
 	return (
 		<section>
-			<h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
-			<p>{currentCategory.description}</p>
-			<div>
-				<img 
-					src={photo}
-					alt='Commercial Example'
-					className='img-thumbnail mx-1'
-				/>
-			</div>
+			<h1 data-testid='h1tag'>{capitalizeFirstLetter(name)}</h1>
+			<p>{description}</p>
+			<PhotoList category={name} />
 		</section>
 	);
 };
