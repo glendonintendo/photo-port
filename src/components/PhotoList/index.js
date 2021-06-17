a9474f612aba5b0e79e4c2d1ae20ac1d7db0ff35
glendo <gleneggers64@gmail.com>
2021-06-17 16:39:10 -0500

    add modal opens when image is clicked

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Modal from '../Modal';
 
 function PhotoList({ category }) {
 	const [photos] = useState([
@@ -98,11 +99,19 @@ function PhotoList({ category }) {
 			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
 		},
 	]);
+	const [currentPhoto, setCurrentPhoto] = useState();
+	const [isModalOpen, setIsModalOpen] = useState(false);
 
 	const currentPhotos = photos.filter(photo => photo.category === category);
 
+	const toggleModal = (image, i) => {
+		setCurrentPhoto({...image, index: i});
+		setIsModalOpen(true);
+	};
+
 	return (
 		<div>
+			{isModalOpen && <Modal currentPhoto={currentPhoto} />}
 			<div className='flex-row'>
 				{currentPhotos.map((image, i) => {
 					return (
@@ -111,6 +120,7 @@ function PhotoList({ category }) {
 						alt={image.name}
 						className='img-thumbnail mx-1'
 						key={image.name}
+						onClick={() => toggleModal(image, i)}
 					/>
 				)})}
 			</div>
