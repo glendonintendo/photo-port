a541c26904e093588f4821a7630a05fd6aa5d5df
glendo <gleneggers64@gmail.com>
2021-06-17 13:36:34 -0500

    add formState; add handlers for form input channges and submit

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,24 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ContactForm() {
+	const [formState, setFormState] = useState({ name: '', email: '', message: ''});
+	const { name, email, message } = formState;
+	
+	const handleChange = (e) => {
+		setFormState({...formState, [e.target.name]: e.target.value });
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(formState);
+	};
+
 	return (
 		<section>
 			<h1>Contact Me</h1>
 			
-			<form id='contact-form'>
+			<form id='contact-form' onSubmit={handleSubmit}>
 				<div>
 					<label htmlFor='name'>Name:</label>
-					<input type ='text' name='name' />
+					<input 
+						type ='text' 
+						name='name' 
+						default={name} 
+						onChange={handleChange}					
+					/>
 				</div>
 
 				<div>
 					<label htmlFor='email'>Email Address:</label>
-					<input type='email' name='email' />
+					<input 
+						type='email' 
+						name='email' 
+						default={email} 
+						onChange={handleChange}	
+					/>
 				</div>
 
 				<div>
 					<label htmlFor='message'>Message:</label>
-					<textarea name='message' rows='5' />
+					<textarea 
+						name='message' 
+						rows='5' 
+						default={message} 
+						onChange={handleChange}	
+					/>
 				</div>
 
 				<button type='submit'>Submit</button>
